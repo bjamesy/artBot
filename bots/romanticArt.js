f882a448b6a40ee667ff51d04d7082ad607dc167
james ballanger <jamesballanger@trentu.ca>
2021-10-17 11:13:13 -0400

    DC friedrich changed into moody landscapes bot

--- bots/romanticArt.js
+++ bots/romanticArt.js
@@ -4,29 +4,40 @@ const { shuffleTags } = require('../db/twitter/tools');
 function romanticism() {    
     const promise = new Promise((resolve, reject) => {
         const hashtags = [
-            '#germanRomanticism', 
-            '#romanticArt', 
-            '#casperDavidFrederich', 
-            '#germanArt', 
-            '#artBot'
+            '#moodyLandscapes', 
+            '#lyricalArt', 
+            '#lyricalLandscapes', 
+            '#art', 
+            '#artbot',
+            '#artTwitter',
+            '#romanticism',
+            '#realism'
         ]
         // function taken from twitter/tools to shuffle hashtags array 
         const tags = shuffleTags(hashtags);
+        const artists = [
+            '57726d97edc2cb3880b4b0fc',  // CD friderich 
+            '57726d88edc2cb3880b49573',  // William turner
+            '57726d88edc2cb3880b494b3',  // Alexei Savrasov
+            '57726d92edc2cb3880b4a829',  // Andrew wyeth
+            '57726d7dedc2cb3880b47dd6'   // john constable
+        ]
+        const artistId = shuffleTags(artists);
 
-        fetch(`https://www.wikiart.org/en/api/2/PaintingsByArtist?id=57726d97edc2cb3880b4b0fc&imageFormat=HD`)
+        fetch(`https://www.wikiart.org/en/api/2/PaintingsByArtist?id=${ artistId[0] }&imageFormat=HD`)
             .then(result => result.json())
             .then(data => {
                 let painting = data.data[Math.floor(Math.random() * data.data.length)];
 
                 let completitionYear = painting.completitionYear.toString();
-                let post = painting.title + "\n".concat(completitionYear);
+                let post = painting.title + ', ' + painting.artistName + "\n".concat(completitionYear);
 
                 resolve({ 
                     rerun: romanticism,
                     content: post + " " + tags[0] + " " + tags[1], 
                     image: painting.image,
                     fileName: "romantic-art.jpg",
-                    name: "Romanticism",
+                    name: "Lyrical Landscapes",
                     consumer_key: process.env.TWITTER_CONSUMER_KEY_romanticism,
                     consumer_secret: process.env.TWITTER_CONSUMER_SECRET_romanticism,
                     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY_romanticism,
